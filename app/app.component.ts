a4750fb124a7c57e07996d11da3e3286fce762be
blcook223 <benjamin.l.cook@gmail.com>
2016-11-22 14:52:57 -0500

    hero service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   styles: [`
@@ -51,6 +52,7 @@ import { Hero } from './hero';
       border-radius: 4px 0 0 4px;
     }`
   ],
+  providers: [HeroService],
   selector: 'my-app',
   template:`
     <h1>{{title}}</h1>
@@ -64,23 +66,17 @@ import { Hero } from './hero';
     `
 })
 export class AppComponent {
-  heroes = HEROES;
+  heroes: Hero[];
   title = 'Tour of Heroes';
   selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
